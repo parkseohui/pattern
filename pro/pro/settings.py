a8f73b8c86c1f5fe5db94730deda3eeb76d3a137
parkseohui <skfhddl003@gmail.com>
2019-03-01 22:24:11 +0900

    디비

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         #'ENGINE': 'django.db.backends.sqlite3',
         #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
          'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'root',
+        'NAME': 'pat',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': 'localhost',
