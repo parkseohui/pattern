861699ff554975f492a588a4f332be1ca3489db7
parkseohui <skfhddl003@gmail.com>
2019-03-01 22:10:58 +0900

    디비

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -75,8 +75,14 @@ WSGI_APPLICATION = 'pro.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_name',
+        'USER': 'db_user',
+        'PASSWORD': 'db_password',
+        'HOST': 'localhost',
+        'PORT':    '3307'
     }
 }
 
